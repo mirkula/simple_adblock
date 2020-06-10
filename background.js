53386ab40d9e6068cbd9847e33dff2f9da2cba0b
Nik Mirkula <mirkula.nik@gmail.com>
2020-06-10 23:11:30 +0300

    Update browser action

--- background.js
+++ background.js
@@ -1,19 +1,64 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(){ return {cancel: true}; },
-  {
-    urls: [
-      "*://franecki.net/*",
-      "*://mrelko.com/*",
-      "*://semi-cod.com/*",
-      "*://semi-cod.com/*",
-      "*://jsc.traffic-media.co.uk/*",
-      "*://z.cdn.trafficbass.com/*",
-      // ads in youtube
-      "*://www.youtube.com/api/stats/ads?*",
-      "*://*.doubleclick.net/*",
-      "*://*.googleadservices.com/*",
-      "*://*.googlesyndication.com/*",
-    ], // Change this to a more specific pattern
-  },
-  ["blocking"]
-);
+var blocked_domains = [
+  "*://franecki.net/*",
+  "*://mrelko.com/*",
+  "*://semi-cod.com/*",
+  "*://semi-cod.com/*",
+  "*://jsc.traffic-media.co.uk/*",
+  "*://z.cdn.trafficbass.com/*",
+  "*://all-cod.com/*",
+  // ads in youtube
+  "*://*.youtube.com/api/stats/ads*",
+  "*://*.youtube.com/get_video_info*",
+  "*://*.doubleclick.net/*",
+  "*://*.googleadservices.com/*",
+  "*://*.googlesyndication.com/*",
+]; // Change this to a more specific pattern
+
+var listener = function(details) {
+  return { cancel: true };
+};
+
+chrome.storage.local.get('enabled', function(data) {
+  if (data.enabled) {
+    chrome.browserAction.setIcon({
+      path: 'on.png'
+    });
+    chrome.webRequest.onBeforeRequest.addListener(
+      listener,
+      {urls: blocked_domains},
+      ["blocking"]
+    );
+  } else {
+    chrome.storage.local.set({enabled: false});
+    chrome.browserAction.setIcon({
+      path: 'off.png'
+    });
+    chrome.webRequest.onBeforeRequest.removeListener(listener);
+  }
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.storage.local.get('enabled', function(data) {
+    if (data.enabled) {
+      chrome.storage.local.set({enabled: false});
+
+      chrome.browserAction.setIcon({
+        path: 'off.png'
+      });
+      chrome.webRequest.onBeforeRequest.removeListener(listener);
+    } else {
+      chrome.storage.local.set({enabled: true});
+
+      chrome.browserAction.setIcon({
+        path: 'on.png'
+      });
+
+      chrome.webRequest.onBeforeRequest.addListener(
+        listener,
+        {urls: blocked_domains},
+        ["blocking"]
+      );
+    }
+    chrome.tabs.reload();
+  })
+});
